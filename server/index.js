3e3f8ac6b495ced54ca242cb6ad5e879f3af00a3
Liam Roberts <liamroberts77@googlemail.com>
2020-11-25 17:36:52 +0000

    beginning the setup of mongodb

--- server/index.js
+++ server/index.js
@@ -9,4 +9,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://liammongo77:<password>@cluster0.iql76.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
 // https://www.mongodb.com/cloud/atlas
+
