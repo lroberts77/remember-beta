3a1d1e4394f93969457468d6297d6d19b66cc6ee
Liam Roberts <liamroberts77@googlemail.com>
2020-11-26 23:44:30 +0000

    set port in index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://liammongo77:<password>@cluster0.iql76.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://liammongo77:liammongo77@cluster0.iql76.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
 
 // https://www.mongodb.com/cloud/atlas
 
