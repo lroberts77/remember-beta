79e95e644415bbfc4e31e58d7f6205a563925351
Liam Roberts <liamroberts77@googlemail.com>
2020-11-26 23:49:42 +0000

    setting up database connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from 'body-parser';
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import cors from "cors";
 
 const app = express();
@@ -11,6 +11,12 @@ app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://liammongo77:liammongo77@cluster0.iql76.mongodb.net/<dbname>?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));  
+
+mongoose.set('useFindAndModify', false);
 
 // https://www.mongodb.com/cloud/atlas
 
