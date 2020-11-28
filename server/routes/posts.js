8efe5ccb5f99c7536064f858d42008c8c63f17d3
Liam Roberts <liamroberts77@googlemail.com>
2020-11-28 17:07:58 +0000

    refactored the groutes posts.jsgetpost logic into controllers posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,9 @@
 import express from 'express';
+import { getPosts } from './controllers/posts.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("THIS WORKS!");
-});
+router.get('/', getPosts);
 
 // http://localhost:5000/posts
 
