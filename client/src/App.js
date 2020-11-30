d6bb47a370ec5977723b4d561c752a22eba19b62
Liam Roberts <liamroberts77@googlemail.com>
2020-11-30 19:12:06 +0000

    fixed typo in material-ui import in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, AppBar, Typography, grow, Grid } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import remember from "./images/remember.jpg";
 
 const App = () => {
