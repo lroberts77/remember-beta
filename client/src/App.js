44f6e17568041d2025ac11577ad9d7cd589875d4
Liam Roberts <liamroberts77@googlemail.com>
2020-12-04 18:58:20 +0000

    added Grids in App.js in client for Form and Posts

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,12 @@ const App = () => {
             <Grow in>
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
                     </Grid>
                 </Container>
             </Grow>
