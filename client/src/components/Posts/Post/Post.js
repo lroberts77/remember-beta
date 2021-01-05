4e296f309c2efc389fcad14bb38258c3acb1549f
Liam Roberts <liamroberts77@googlemail.com>
2021-01-05 19:34:10 +0000

    fixed typos to image div

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import useStyles from './styles';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
-import ThumbUpAltIcon from '@materila-ui/icons/ThumbUpAlt';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
 
 
 
-const Post = () => {
+const Post = ({ post }) => {
     const classes = useStyles();
     return (
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image={postMessage.selectedFile} title={postMessage.title} />
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
             <div className={classes.overlay}>
-                <Typography variant="h6">{post.Typography.creator}</Typography>
+                <Typography variant="h6">{post.creator}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
 
             </div>
