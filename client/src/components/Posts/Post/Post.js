30b9066262669337b59eb2bc3f014da11fc40132
Liam Roberts <liamroberts77@googlemail.com>
2021-01-05 18:56:10 +0000

    added a div to display and add a # to all tags

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -17,6 +17,14 @@ const Post = () => {
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
 
             </div>
+            <div className={classes.overlay2}>
+                <Button style={{color: 'white'}} size="small" onClick={() => {}}>
+                    <MoreHorizIcon fontSize="default" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `</Typography>   
+            </div>
         </Card>
     );
 }
