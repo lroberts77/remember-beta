feb87a38c440947ea389d8da1b5c28f40a28dc81
Liam Roberts <liamroberts77@googlemail.com>
2021-01-05 19:46:58 +0000

    fixed typo on closing div moved classes.details ending div to under first typography which fixed card layout

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -16,7 +16,6 @@ const Post = ({ post }) => {
             <div className={classes.overlay}>
                 <Typography variant="h6">{post.creator}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
-
             </div>
             <div className={classes.overlay2}>
                 <Button style={{color: 'white'}} size="small" onClick={() => {}}>
@@ -25,6 +24,7 @@ const Post = ({ post }) => {
             </div>
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)} </Typography> 
+            </div>
                 <CardContent>
                 <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>   
                 </CardContent>
@@ -39,7 +39,6 @@ const Post = ({ post }) => {
                         Delete
                     </Button>
                 </CardActions>
-            </div>
         </Card>
     );
 }
