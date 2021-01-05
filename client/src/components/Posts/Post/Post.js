26e9c24ce170909cb08658bea47a8336d7fa49be
Liam Roberts <liamroberts77@googlemail.com>
2021-01-05 17:55:04 +0000

    imported thumbupalticon from material-ui

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import useStyles from './styles';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltIcon from '@materila-ui/icons/ThumbUpAlt';
+
 
 const Post = () => {
     const classes = useStyles();
