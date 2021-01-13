2c47bb6837bb57e2c44d9f5508152ccf66b96581
Liam Roberts <liamroberts77@googlemail.com>
2021-01-13 13:31:58 +0000

    changed post message color and size to give distinction from post title

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -34,7 +34,7 @@ const Post = ({ post, setCurrentId }) => {
             </div>
             <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>   
                 <CardContent>
-                <Typography variant="h5" gutterBottom>{post.message}</Typography>   
+                <Typography variant="body2" color="secondary" gutterBottom>{post.message}</Typography>   
                 </CardContent>
                 <CardActions className={classes.cardActions}>
                     <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
