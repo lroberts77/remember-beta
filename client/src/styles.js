436883fe06eaa21e960fdd2ae0f1d61c84b0d373
Liam Roberts <liamroberts77@googlemail.com>
2021-01-13 17:24:46 +0000

    resized heading on smaller devices in client/styles.js

--- client/src/styles.js
+++ client/src/styles.js
@@ -18,6 +18,9 @@ export default makeStyles((theme) => ({
   [theme.breakpoints.down('sm')]: {
     mainContainer: {
       flexDirection: "column-reverse"
-    }
+    },
+    heading: {
+    fontSize: '1.6rem'
+    },
   }
 }));
