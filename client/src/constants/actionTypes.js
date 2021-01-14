faab856be6b201896aef36fb0492606849e86655
Liam Roberts <liamroberts77@googlemail.com>
2021-01-14 17:44:50 +0000

    exported all const in actiontypess

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,6 +1,6 @@
-const FETCH_ALL = 'FETCH_ALL';
-const CREATE = 'CREATE';
-const UPDATE =  'UPDATE';
-const DELETE = 'DELETE';
+export const FETCH_ALL = 'FETCH_ALL';
+export const CREATE = 'CREATE';
+export const UPDATE =  'UPDATE';
+export const DELETE = 'DELETE';
 
 
