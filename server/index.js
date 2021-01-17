21e40798bedc8cc7b856768c4beb99be9ece28c1
Liam Roberts <liamroberts77@googlemail.com>
2021-01-17 17:59:31 +0000

    added dotenv.config in index.js

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
