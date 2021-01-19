f2ec7d60be4451fafa2af47adc4679c3374bec46
Liam Roberts <liamroberts77@googlemail.com>
2021-01-19 18:23:21 +0000

    fixed typo backend deployed and update post now working

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import mongoose from "mongoose";
-import PostMessage from "../models/postMessage.js";
+import mongoose from 'mongoose';
+import PostMessage from '../models/postMessage.js';
 
 const router = express.Router();
 
@@ -47,9 +47,7 @@ export const updatePost = async (req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
-
-    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true }); 
 
     res.json(updatedPost);
 }
--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import bodyParser from 'body-parser';
-import mongoose from "mongoose";
-import cors from "cors";
+import mongoose from 'mongoose';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
@@ -25,6 +25,3 @@ mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnified
     .catch((error) => console.log(error.message));  
 
 mongoose.set('useFindAndModify', false);
-
-// https://www.mongodb.com/cloud/atlas
-
