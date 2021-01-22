db7056975f9a78f68b0e61042389dacc425d838f
Liam Roberts <liamroberts77@googlemail.com>
2021-01-22 23:05:39 +0000

    added frosted glass styling to card in Posts/Post/styles

--- client/src/styles.js
+++ client/src/styles.js
@@ -11,6 +11,8 @@ export default makeStyles((theme) => ({
     backgroundColor: "rgba(255, 255, 255, 0.1)",
     boxShadow: "20px 20px 50px rgba(0, 0, 0, 0.5)",
     backdropFilter: "blur(5px)",
+    borderTop: "1px solid rgba(255, 255, 255, 0.5)",
+    borderLeft: "1px solid rgba(255, 255, 255, 0.5)",
   },
   heading: {
     color: 'rgba(0,183,255, 1)',
